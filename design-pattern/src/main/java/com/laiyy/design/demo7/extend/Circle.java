fd16b7d3511fc559eed0aef7963ba6b301aa5a03
laiyy <laiyy0728@gmail.com>
2018-08-17 16:29:09 +0800

    桥接模式

--- design-pattern/src/main/java/com/laiyy/design/demo7/extend/Circle.java
+++ design-pattern/src/main/java/com/laiyy/design/demo7/extend/Circle.java
@@ -7,6 +7,7 @@ import com.laiyy.design.demo7.Shape;
  * @author laiyy
  * @date 2018/8/17 16:23
  * @description
+ * 通过该实体类，将 redCircle、greenCircle 桥接到 DrawAPI 上。
  */
 public class Circle extends Shape {
 
