76dd987bf0867efc955d3c6778a81d71cfe7edab
laiyy0728 <laiyy0728@gmail.com>
2018-11-30 17:21:17 +0800

    init

---
+++ cloud_java/src/main/java/com/laiyy/cloud_java/CloudJavaApplication.java
@@ -0,0 +1,12 @@
+package com.laiyy.cloud_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudJavaApplication.class, args);
+    }
+}
